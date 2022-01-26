9242252a86694cb53b6f97ef48a7cbcd7e4a98be
Jksingh-13 <97751934+Jksingh-13@users.noreply.github.com>
2022-01-26 21:29:03 +0530

    Add files via upload

---
+++ 21103055_Assignment2_sc.py
@@ -0,0 +1,75 @@
+#Ans 1
+N = input("Enter the string")
+#1a
+print("The length of string is ",len(N))
+#1b
+print("The reversed string is ",N[::-1])
+#1c
+N1= N[10:26:]
+print("The index of substring A is ",N1)
+#1d
+N2=N[:10:]+"object oriented",N[26::]
+print("The original string after removing whitespaces is ",N2)
+#1e
+x=N.index("a")
+print(x)
+#1f
+N3=N.replace(" ","")
+print(N3)
+print()
+
+
+#Ans 2
+Name="Jaskeerat Singh"
+Sid=21103055
+Department="CSE"
+CGPA=9.8
+print("Hey, %s Here!"%Name)
+print("My SID is %d"%Sid)
+print("I am from %s department and my CGPA is %.2f"%(Department,CGPA))
+print()
+
+#Ans 3
+a=56 
+b=10
+print("a&b= " ,a&b)
+print("a|b= " ,a|b)
+print("a^b= " ,a^b)
+print("Left shift both a and b with 2 bits respectively are :",a<<2 , b<<2)
+print("Right shift a with 2 bits and b with 4 bits respectively are : " ,a>>2 , b>>4)
+print()
+
+#Ans 4
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+c=int(input("Enter third number: "))
+arr=[a,b,c]
+arr.sort()
+print(arr[-1])
+print()
+
+#Ans 5
+string=input("Enter a string")
+res=False
+for i in range(len(string)):
+	if string[i]=="n":
+		if string[i:i+4]=="name":
+			print("yes")
+			res=True
+if not res:
+	print("no")
+print()
+
+#Ans 6
+length1=input("Enter first side of triangle")
+length2=input("Enter second side of triangle")
+length3=input("Enter third side of triangle")
+length1,length2,length3=int(length1),int(length2),int(length3)
+if length1+length2<length3:
+	print("NO")
+elif length2+length3<length1:
+	print("NO")
+elif length1+length3<length2:
+	print("NO")
+else:
+	print("YES")
---
+++ 21103055_Assignment_output.py
@@ -0,0 +1,34 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========= RESTART: C:/Users/Jaskeerat/OneDrive/Documents/Output_Assign 2.py =========
+Enter the string python is a case sesative language
+The length of string is  35
+The reversed string is  egaugnal evitases esac a si nohtyp 
+The index of substring A is   a case sesative
+The original string after removing whitespaces is  (' python isobject oriented', ' language')
+11
+pythonisacasesesativelanguage
+
+Hey, Jaskeerat Singh Here!
+My SID is 21103055
+I am from CSE department and my CGPA is 9.80
+
+a&b=  8
+a|b=  58
+a^b=  50
+Left shift both a and b with 2 bits respectively are : 224 40
+Right shift a with 2 bits and b with 4 bits respectively are :  14 0
+
+Enter first number: 41
+Enter second number: 78
+Enter third number: 24
+78
+
+Enter a string Jaskeerat
+no
+
+Enter first side of triangle 54
+Enter second side of triangle 34 
+Enter third side of triangle 84
+YES
